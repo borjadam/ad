c0f9e8683c0f7b3c177ae749dcc80fb63eec9430
borja <bma.francisco@gmail.com>
2012-11-21 08:59:47 +0100

    añadiendo PComboBox

---
+++ PComboBox/PComboBox/MainWindow.cs
@@ -0,0 +1,82 @@
+using System;
+using Gtk;
+
+public delegate int MyFunction (int a, int b);
+
+public partial class MainWindow: Gtk.Window
+{	
+	private ListStore listStore;
+	public MainWindow (): base (Gtk.WindowType.Toplevel)	
+		
+	{
+		Build ();
+		
+		MyFunction f;
+		
+		MyFunction[] functions = new MyFunction[]{suma, resta, multiplica};
+		int random = new Random().Next(3);
+		f = functions[random];
+		
+		
+//		if (new Random().Next(2) ==0){
+//			f=suma;
+//		}
+//		else{
+//			f=resta;
+//		}
+		
+		Console.WriteLine("f={0}", f(5,3));
+		
+		//comboBox
+		CellRenderer cellRenderer = new CellRendererText();
+		combobox.PackStart(cellRenderer, false);
+		combobox.AddAttribute (cellRenderer, "text", 1);
+		
+		listStore = new ListStore(typeof(string), typeof(string));
+		combobox.Model = listStore;
+		
+		listStore.AppendValues("1", "Uno");
+		listStore.AppendValues("2", "Dos");
+		
+		combobox.Changed += delegate {
+			
+			TreeIter treeIter;
+			
+			if (combobox.GetActiveIter (out treeIter)) {  //item seleccionado
+				object value = listStore.GetValue(treeIter, 0);
+				Console.WriteLine ("ComboBox.Changed delegate id={0}", value);
+			}
+				
+		};
+		
+		combobox.Changed += comboBoxChanged;		
+	}
+	
+	private int suma (int a, int b) {
+			return a+b;
+		}
+	
+	private int resta (int a, int b) {
+			return a-b;
+		}
+	private int multiplica (int a, int b) {
+			return a*b;
+		}
+	
+	private void comboBoxChanged(object obj, EventArgs args) {
+		
+			TreeIter treeIter;
+			
+			if (combobox.GetActiveIter (out treeIter)) {  //item seleccionado
+				object value = listStore.GetValue(treeIter, 0);
+				Console.WriteLine ("ComboBox.Changed id={0}", value);
+			}
+	}
+	
+	
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+}
