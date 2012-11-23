6856b89161fbfdb3301968dac35354cd5ba838d9
borja <bma.francisco@gmail.com>
2012-11-23 09:35:34 +0100

    cambiando PCategoria

--- PCategoria/PCategoria/MainWindow.cs
+++ PCategoria/PCategoria/MainWindow.cs
@@ -1,11 +1,42 @@
 using System;
 using Gtk;
+using System.Data;
+using Npgsql;
 
 public partial class MainWindow: Gtk.Window
 {	
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		fillComboBox();
+	}
+	
+	private void fillComboBox() {
+		
+		CellRenderer cellRenderer = new CellRendererText();
+		comboBox.PackStart(cellRenderer, false);
+		comboBox.AddAttribute(cellRenderer, "text", 1);
+		
+		ListStore listStore = new ListStore(typeof(string), typeof(string));
+		
+		comboBox.Model = listStore;
+		
+		String connectionString = 
+			"Server= localhost; Database= dbprueba; User Id=dbprueba; Password = 12345";
+		IDbConnection dbConnection = new NpgsqlConnection(connectionString);
+		
+		IDbCommand dbCommand = dbConnection.CreateCommand();
+		dbCommand.CommandText= "select id, nombre from categoria";
+		
+		IDataReader dataReader = dbCommand.ExecuteReader();
+		
+		while (dataReader.Read()) {
+			listStore.AppendValues(dataReader["id"].ToString(), dataReader["nombre"].ToString());
+		}
+		
+		dataReader.Close();
+		dbConnection.Close();
+		
 	}
 	
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
