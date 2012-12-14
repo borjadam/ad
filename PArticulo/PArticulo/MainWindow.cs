01990255ba24be69a60dddd2e54f7a016a516209
borja <bma.francisco@gmail.com>
2012-12-14 08:29:24 +0100

    añadiendo PArticulo

---
+++ PArticulo/PArticulo/MainWindow.cs
@@ -0,0 +1,76 @@
+using Gtk;
+using Npgsql;
+using PArticulo;
+using Serpis.Ad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+
+public partial class MainWindow: Gtk.Window
+{
+	private IDbConnection dbConnection;
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		
+		string connectionString = "Server=localhost;Database=dbprueba;User Id=dbprueba;Password=12345";
+		ApplicationContext.Instance.DbConnection = new NpgsqlConnection(connectionString);
+		dbConnection = ApplicationContext.Instance.DbConnection;
+		dbConnection.Open ();
+		
+		IDbCommand dbCommand = dbConnection.CreateCommand ();
+		dbCommand.CommandText = 
+			"select a.id, a.nombre, a.precio, categoria " +
+			"from articulo a left join categoria c " +
+			"on a.categoria = c.id";
+		
+		IDataReader dataReader = dbCommand.ExecuteReader ();
+		
+		TreeViewExtensions.Fill (treeView, dataReader);
+		dataReader.Close ();
+		
+		dataReader = dbCommand.ExecuteReader ();
+		TreeViewExtensions.Fill (treeView, dataReader);
+		dataReader.Close ();
+		
+	}
+	
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		dbConnection.Close ();
+
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnClearActionActivated (object sender, System.EventArgs e)
+	{
+		ListStore listStore = (ListStore)treeView.Model;
+		listStore.Clear ();
+	}
+
+	protected void OnEditActionActivated (object sender, System.EventArgs e)
+	{
+		showArticulo ( getSelectedId() );
+	}
+	
+	private long getSelectedId() {
+		TreeIter treeIter;
+		treeView.Selection.GetSelected(out treeIter);
+		
+		ListStore listStore = (ListStore)treeView.Model;
+		return long.Parse (listStore.GetValue (treeIter, 0).ToString ()); 
+	}
+
+	protected void OnNewActionActivated (object sender, System.EventArgs e)
+	{
+		showArticulo (0); 
+	}
+	
+	private void showArticulo(long id)
+	{
+		ArticuloView articuloView = new ArticuloView( id );
+		articuloView.Show ();
+	}
+}
